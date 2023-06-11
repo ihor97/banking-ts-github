3936625f26b0e5f26057bb14bf7bc95160f49771
ihor97 <daniel.waffner@gmail.com>
2023-06-11 21:51:22 +0100

    private static

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,17 @@
-"use strict";
-let g = 5;
+class Car {
+    constructor(a, b = 'стандарт') {
+        this.name = a;
+        this.number = b;
+    }
+    getInfo() {
+        return `машина ${this.name}, с госномером ${this.number} `;
+    }
+    static compare(car1, car2) {
+        Car._i++;
+        return car1.name === car2.name && car1.number === car2.number;
+    }
+}
+let car1 = new Car('cddc', 'первая');
+let car2 = new Car('cddвввc', 'вторая');
+console.log(Car.compare(car1, car2));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,27 @@
-let g:number=5
- 
+class Car {
+    public number: string
+    public name: string
+    // приватне статичне поле можна використовувати тоді коли ми в рамках класах хочемо інкапсулювати логіку створених ним обєктів
+    private static _array: Car[]
+    constructor(a: string, b: string = 'стандарт') {
+        this.name = a
+        this.number = b
+    }
+    public getInfo(): string {
+        return `машина ${this.name}, с госномером ${this.number} `
+    }
+    public static create(a: string, b: string = 'стандарт'): Car {
+        let c = new Car(a, b)
+        this._array.push(c)
+        return c
+    }
+   
+    public static compare(car1: Car, car2: Car): boolean {
+        return car1.name === car2.name && car1.number === car2.number
+
+    }
+}
+let car1 = new Car('cddc', 'первая')
+let car2 = new Car('cddвввc', 'вторая')
+
+console.log(Car.compare(car1, car2));
