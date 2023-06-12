287cd193e1a3e9887d0935329fe45fcaa1b0479e
ihor97 <daniel.waffner@gmail.com>
2023-06-12 21:43:40 +0100

    наследование

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,51 @@
-class Car {
-    set name(g) {
-        // можна робити форматування this._name = g+'eee'
-        this._name = g;
+// this && this.__extends перевірка на this і чи є властивість __extends
+var __extends = (this && this.__extends) || (function () {
+
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+// створення ф-ї Object.setPrototypeOf
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        // якщо не ф-я то ексепшн
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    //    
+            extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var classBase = /** @class */ (function () {
+    function classBase(p1, p2, p3) {
+        this.property1 = p1;
+        this.property2 = p2;
+        this._property3 = p3;
     }
-    get name() {
-        // заьираємо добавлені значення this._name.replace('aaa','')
-        return this._name;
+    classBase.prototype.getInfo = function () {
+        return "prop1 ".concat(this.property1, " prop2 ").concat(this.property2, " prop3 ").concat(this._property3);
+    };
+    return classBase;
+}());
+var ClassA = /** @class */ (function (_super) {
+    __extends(ClassA, _super);
+    function ClassA() {
+        // super це є classBase
+        return _super.call(this, 1, 'класс А', true) || this;
     }
-    constructor(a) {
-        this._name = a;
+    return ClassA;
+}(classBase));
+var ClassB = /** @class */ (function (_super) {
+    __extends(ClassB, _super);
+    function ClassB() {
+        return _super.call(this, 1, 'класс B', false) || this;
     }
-}
-const a = new Car('1');
-console.log(a.name);
-//# sourceMappingURL=app.js.map
+    return ClassB;
+}(classBase));
+var A = new ClassA();
+var B = new ClassB();
+console.log(A.getInfo());
+console.log(B.getInfo());
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,29 @@
-class Car {
-    // тут в чистому вигляді інкапсуляція поля _name
-    private _name: string
-
-    public set name(g: string) {
-        // можна робити форматування this._name = g+'eee'
-        this._name = g
+class classBase{
+    public property1:number
+    protected property2:string
+    private _property3:boolean
+    constructor(p1:number,p2:string,p3:boolean){
+        this.property1=p1
+        this.property2=p2
+        this._property3=p3
     }
-    public get name() {
-        // заьираємо добавлені значення this._name.replace('aaa','')
-
-        return this._name
+    public getInfo():string{
+        return `prop1 ${this.property1} prop2 ${this.property2} prop3 ${this._property3}`
     }
-    constructor(a: string) {
-        this._name = a
-    }
-
-
 }
 
-const a = new Car('1')
+class ClassA extends classBase{
+    constructor(){
+        super(1,'класс А',true)
+    }
+}
+class ClassB extends classBase{
+    constructor(){
+        super(1,'класс B',false)
+    }
+}
 
-console.log(a.name);
+const A=new ClassA()
+const B=new ClassB()
+console.log(A.getInfo());
+console.log(B.getInfo());
