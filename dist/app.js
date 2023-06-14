eabf9bec0dcde9c26a56c933a8aa75e2c4898b96
ihor97 <daniel.waffner@gmail.com>
2023-06-14 19:29:58 +0100

    enum

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,10 @@
-// це різні типи 
-// let o2:object=1 //тут тільки обєкти
-// ці майже подібні
-// let o1:{}=1
-// Object це є тип з TS
-// let o3:Object=undefined // тут можна пхати все
-// strictNullChecks - не можна присвоїти null буде 
+var User;
+(function (User) {
+    User[User["Admin"] = 0] = "Admin";
+    User[User["Client"] = 1] = "Client";
+})(User || (User = {}));
+// використання єнамів, єнам це перечислення
+let a = User.Admin;
+// ще можна записати ось так число що відповідає значенню з єнама
+let b = 1;
 //# sourceMappingURL=app.js.map
