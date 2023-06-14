7c1e31a85b89160f98ba7b2d29881aea6d34dd2e
ihor97 <daniel.waffner@gmail.com>
2023-06-14 22:02:08 +0100

    типи  ф-ї і перегрузки

--- dist/app.js
+++ dist/app.js
@@ -1,61 +1,14 @@
-const persons = [
-    {
-        type: 'user',
-        name: 'Max Musterman',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        type: 'admin',
-        name: 'Jane Joe',
-        age: 27,
-        role: 'Administrator'
-    },
-    {
-        type: 'admin',
-        name: 'Willis',
-        age: 64,
-        role: 'World saver'
-    },
-    {
-        type: 'user',
-        name: 'Wilson',
-        age: 64,
-        occupation: 'ball'
-    },
-    {
-        type: 'admin',
-        name: 'Agent Smith',
-        age: 23,
-        role: 'Administrator'
-    }
-];
-const isAdmin = (person) => person.type === 'admin';
-const isUser = (person) => person.type === 'user';
-function logPerson(person) {
-    let additionalInfo = '';
-    if (isAdmin(person)) {
-        additionalInfo = person.role;
-    }
-    if (isUser(person)) {
-        additionalInfo = person.occupation;
-    }
-    console.log(`- ${person.name},${person.age},${additionalInfo}`);
+// типи ф-й
+// делегат - силка на ф-ю
+// запис ф-й тобто робимо функцію деякого типу
+// const add:(a:number,b:number)=>number=function (a:number,b:number) {
+//     return a+b
+// }
+function add(x, y) {
+    return x + y;
 }
-// можна зробити обмеження для приймаємого типу 
-// Pick дозволяє вибрати декілька параметрів з обєкта і привести його до деякого типу
-// Pick в даній області видимості дозволяє юзати тип не як повноцінний тип User а як тільки обєкт з одним полем age при тому він розглядається як тип юзер
-//   criteria:Pick<User,'age'> Pick  бере з обєкта тільки поле age
-function filterUser(persons, criteria) {
-    return persons.filter(isUser).filter(user => {
-        // The keyof operator takes an object type and produces a string or numeric literal union of its keys.
-        const criteriaKeys = Object.keys(criteria);
-        return criteriaKeys.every(fieldName => {
-            return user[fieldName] === criteria[fieldName];
-        });
-    });
-}
-console.log(filterUser(persons, {
-    age: 25
-}));
+// Ctrl+Shift+Space і треба поставити курсор всередині дужок метода тоді можна получити перегрузки які є в ф-ї
+// перегрузка мождива тому що це все компілиться в js і всі типи пропадають 
+let res = add(1, 2);
+let res1 = add('1', '2');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,81 +1,69 @@
-interface User {
-    type: 'user';
-    name:string;
-    age:number;
-    occupation:string
-}
-interface Admin {
-    type: 'admin';
-    name:string;
-    age:number;
-    role:string
-}
+// типи ф-й
+// делегат - силка на ф-ю
+// запис ф-й тобто робимо функцію деякого типу
+// const add:(a:number,b:number)=>number=function (a:number,b:number) {
+//     return a+b
+// }
 
- type Person=User | Admin
+// тип для ф-ї
 
- const persons:Person[]=[
-    {
-        type:'user',
-        name:'Max Musterman',
-        age:25,
-        occupation:'Chimney sweep'
-    },
-    {
-        type:'admin',
-        name:'Jane Joe',
-        age:27,
-        role:'Administrator'
-    },
-    {
-        type:'admin',
-        name:'Willis',
-        age:64,
-        role:'World saver'
-    },
-    {
-        type:'user',
-        name:'Wilson',
-        age:64,
-        occupation:'ball'
-    },
-    {
-        type:'admin',
-        name:'Agent Smith',
-        age:23,
-        role:'Administrator'
-    }
-]
+// type typeAdd1=(a:number, b:number)=>string
+// type typeAdd2=(a:number, b:string)=>string
 
- const isAdmin=(person:Person):person is Admin=>person.type==='admin'
- const isUser=(person:Person):person is User=>person.type==='user'
+// const add:typeAdd1=function (a:number,b:number) {
+//     return (a+b).toString()
+// }
 
- function logPerson(person:Person) {
-    let additionalInfo=''
-    if(isAdmin(person)){
-        additionalInfo=person.role
-    }
-    if(isUser(person)){
-        additionalInfo=person.occupation
-    }
-    console.log(`- ${person.name},${person.age},${additionalInfo}`);
+// let veryfyType:boolean
+
+// if (add as typeAdd1){
+//     console.log(add(1,2));
     
+// }
+
+
+// type typeFuncprint=(a:number,b:number,c:number)=>void
+// let p:typeFuncprint=function (a:number,b:number,c:number) {
+//     console.log(`${a}+${b}+${c}`);
+// }
+// // print:typeFuncprint мусить приймати ф-ю з таким типом
+// function previewCalculate(start:number[],print:typeFuncprint) {
+//     start.forEach(a=>{
+//         start.forEach(b=>{
+//             print(a,b,a+b)
+//         })
+//     })
+// }
+// let g:()=>void=function () {
+//     console.log('dd');
+    
+// }
+// передаємо силку на ф-ю 
+// previewCalculate([1,2,3,4,5],p)
+
+
+// необовязкові параметри
+// function getname(str:string,str1?:string,str2?:string) {
+//     return str +' '+ str1+' '+str2
+// }
+
+// function addnumbers(a:number,...arrays:number[]):number {
+//     let res=a
+//     arrays.forEach(n=>{
+//         res+=n
+//     })
+//     return res
+// }
+// let num1=addnumbers(1,2,3,4)
+// console.log(num1);
+
+// перегрузка ф-ї
+function add(x:string,y:string):string;
+function add(x:number,y:number) :number;
+function add(x:any,y:any):any {
+    return x+y
 }
-// можна зробити обмеження для приймаємого типу 
-// Pick дозволяє вибрати декілька параметрів з обєкта і привести його до деякого типу
-// Pick в даній області видимості дозволяє юзати тип не як повноцінний тип User а як тільки обєкт з одним полем age при тому він розглядається як тип юзер
-//   criteria:Pick<User,'age'> Pick  бере з обєкта тільки поле age
-function filterUser(persons:Person[],criteria:Pick<User,'age'>):User[] {
-    return persons.filter(isUser).filter(user=>{
-        // The keyof operator takes an object type and produces a string or numeric literal union of its keys.
-        const criteriaKeys=Object.keys(criteria) as (keyof User)[]
-        return criteriaKeys.every(fieldName=>{
-            return user[fieldName]===criteria[fieldName]
-        })
-    })
-}
-console.log(filterUser(
-    persons,
-    {
-        age:25
-    }
-));
+// Ctrl+Shift+Space і треба поставити курсор всередині дужок метода тоді можна получити перегрузки які є в ф-ї
+// перегрузка мождива тому що це все компілиться в js і всі типи пропадають 
+let res=add(1,2)    
+let res1=add('1','2')
