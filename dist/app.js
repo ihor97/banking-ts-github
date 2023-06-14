079f588a53b5d245affcf47bfade4048c36e5071
ihor97 <daniel.waffner@gmail.com>
2023-06-14 19:19:29 +0100

    object Object {}

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,8 @@
-"use strict";
-let g = 5;
+// це різні типи 
+// let o2:object=1 //тут тільки обєкти
+// ці майже подібні
+// let o1:{}=1
+// Object це є тип з TS
+// let o3:Object=undefined // тут можна пхати все
+// strictNullChecks - не можна присвоїти null буде 
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+
+// це різні типи 
+// let o2:object=1 //тут тільки обєкти
+// ці майже подібні
+// let o1:{}=1
+// Object це є тип з TS
+// let o3:Object=undefined // тут можна пхати все
+
+// strictNullChecks - не можна присвоїти null буде 
+
