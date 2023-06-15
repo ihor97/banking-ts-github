89026ce0be46972b58c56f85705b28ec7772797f
ihor97 <daniel.waffner@gmail.com>
2023-06-15 20:02:46 +0100

    поліморфізм з метода базового класу

--- dist/app.js
+++ dist/app.js
@@ -1,52 +1,39 @@
-// поліморфізм якийсь обєкт в різних умовах може мати різні властивості при тому маючи один інтерфейс
-// може бути один метод і в залежності від того в якому класі ми його викликаємо з одною й тою самою сигнатурою виконує різні ф-ї
-class PrintMessageToConsole {
+class Control {
     constructor() {
-        this._prefix = 'вывод многострочный';
     }
-    saveMessage(message) {
-        if (!message) {
-            return false;
-        }
-        this._message = message;
-        return true;
-    }
-    print() {
-        if (!this._message) {
-            throw new Error('пусто!');
-        }
-        let text = `
-        ${this._prefix}.
-        Сообщение: '${this._message}'`;
-        console.log(text);
+    // у нас є три різні методи getinfo
+    getinfo() {
+        return `Обычная иформация о контроле`;
     }
 }
-class PrintMessagToDocument {
-    constructor() { }
-    saveMessage(message) {
-        if (!message) {
-            return false;
-        }
-        this._message = 'Вывод в дом в одну строку.';
-        return true;
+class TextBox extends Control {
+    constructor() {
+        super();
     }
-    print() {
-        document.write(this._message);
+    // поліморфізм
+    getinfo() {
+        return `Инфа о контроле TextBox`;
     }
 }
-const printMessageToConsole = new PrintMessageToConsole();
-if (printMessageToConsole.saveMessage('сообщение для класа printMessageToConsole')) {
-    printMessageToConsole.print();
+class SelectBox extends Control {
+    constructor() {
+        super();
+    }
 }
-const printMessageToDocument = new PrintMessagToDocument();
-if (printMessageToDocument.saveMessage('сообщение для класа printMessageToDocument')) {
-    printMessageToDocument.print();
+class CheckBox extends Control {
+    constructor() {
+        super();
+    }
+    // поліморфізм
+    getinfo() {
+        let baseInfo = super.getinfo();
+        return `${baseInfo}, а также инфа с CheckBox`;
+    }
 }
-// маємо ф-ю яка приймає обєкт типу IExample  в залежності від того який обєкт передамо буде інший вивід
-// маємо ф-ю яка буде приймати два типи наших обєктів по типу інтерфейса
-function showObj(obj) {
-    obj.print();
-}
-// showObj(printMessageToDocument)
-// showObj(printMessageToConsole)
+const textBox = new TextBox();
+console.log(textBox.getinfo());
+const selectBox = new SelectBox();
+console.log(selectBox.getinfo());
+const checkBox = new CheckBox();
+console.log(checkBox.getinfo());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,46 @@
-// поліморфізм якийсь обєкт в різних умовах може мати різні властивості при тому маючи один інтерфейс
-// може бути один метод і в залежності від того в якому класі ми його викликаємо з одною й тою самою сигнатурою виконує різні ф-ї
 
-// є дві варіації - коли один інтерфейс має багато реалізацій і другий коли один метод в залежності від того 
-// з якого класу ми його викликаємо з одною і тою самою сигнатурою на одному дереві наслідування він виконує різні функції
-
-interface IPrintMessage{
-    saveMessage(message:string):boolean
-    print():void
+interface IControl{
+    getinfo:()=>string
 }
-class PrintMessageToConsole implements IPrintMessage{
-    private _prefix:string
-    private _message:string
-
+class Control implements IControl{
     constructor(){
-        this._prefix='вывод многострочный'
-    }
 
-    public saveMessage(message: string): boolean {
-        if(!message){
-            return false
-        }
-        this._message=message
-        return true
     }
-    public print(): void {
-        if(!this._message){
-            throw new Error('пусто!')
-        }
-        let text=`
-        ${this._prefix}.
-        Сообщение: '${this._message}'`
-        console.log(text);
-        
-    }
-}
-class PrintMessagToDocument implements IPrintMessage{
-    private _message:string
-    constructor(){}
-    public saveMessage(message: string): boolean {
-        if(!message){
-            return false    
-        }
-        this._message='Вывод в дом в одну строку.'
-        return true
-    }
-    public print(): void {
-        document.write(this._message)
+    // у нас є три різні методи getinfo
+    public getinfo():string{
+        return `Обычная иформация о контроле`
     }
 }
 
-const printMessageToConsole= new PrintMessageToConsole()
-if(printMessageToConsole.saveMessage('сообщение для класа printMessageToConsole')){
-    printMessageToConsole.print()
+class TextBox extends Control{
+    constructor(){
+        super()
+    }
+    // поліморфізм
+    public getinfo(): string {
+        return `Инфа о контроле TextBox`
+    }
 }
-const printMessageToDocument= new PrintMessagToDocument()
-if(printMessageToDocument.saveMessage('сообщение для класа printMessageToDocument')){
-    printMessageToDocument.print()
+class SelectBox extends Control{
+    constructor(){
+        super()
+    }
+}
+class CheckBox extends Control{
+    constructor(){
+        super()
+    }
+    // поліморфізм
+    public getinfo(): string {
+        let baseInfo=super.getinfo()
+        return `${baseInfo}, а также инфа с CheckBox`
+    
+    }
 }
 
-// маємо ф-ю яка приймає обєкт типу IExample  в залежності від того який обєкт передамо буде інший вивід
-// маємо ф-ю яка буде приймати два типи наших обєктів по типу інтерфейса
-function showObj(obj:IPrintMessage){
-    obj.print()
-}
-
-// showObj(printMessageToDocument)
-// showObj(printMessageToConsole)
+const textBox=new TextBox()
+console.log((<IControl>textBox).getinfo());
+const selectBox=new SelectBox()
+console.log((<IControl>selectBox).getinfo());
+const checkBox=new CheckBox()
+console.log((<IControl>checkBox).getinfo());
