a14102131c071896ccdc289bc1cad35a045bd1b0
ihor97 <daniel.waffner@gmail.com>
2023-06-15 19:27:49 +0100

    повторка

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,17 @@
-"use strict";
-let g = 5;
+// super.getInfo()  виклик метода базового класу
+// можна наслідуватися тільки від класу так що від enum ми не можемо наслідуватися
+// class Car{
+//     public name:string
+//     public power:number
+//     constructor(a:string,b:number){
+//         this.name=a
+//         this.power=b
+//     }
+//     // тут можемо перевизначити конструктор трохи колхозний метод
+//     public static createCar(a:string){
+//         let g =new Car('',2)
+//         g.name=a
+//         return g
+//     }
+// }
 //# sourceMappingURL=app.js.map
