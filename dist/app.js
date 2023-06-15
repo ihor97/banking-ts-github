6813719ece01c27740a53a0f77e4f6d8078cfda3
ihor97 <daniel.waffner@gmail.com>
2023-06-15 20:41:20 +0100

    приведення типів обєктів

--- dist/app.js
+++ dist/app.js
@@ -1,26 +1,43 @@
-class Control {
-    constructor() { }
+// приведення  типів інстансів класів можна робити через instaceof 
+// instanceof для інтерфейсів не працює так як в js немає інтерфейсів
+class Example {
+    convert() {
+        return `name ${this.name} count=${this.count}`;
+    }
 }
-// текстбокси працюють тільки з string
-class TextBox extends Control {
-    constructor() {
+class ExampleA extends Example {
+    constructor(a) {
         super();
-    }
-    setValue(val) {
-        this.value = val;
+        this.testA = a;
     }
 }
-// чекбокси працюють тільки з boolean
-class CheckBox extends Control {
-    constructor() {
+class ExampleB extends Example {
+    constructor(b) {
+        // в конструкторі потомків завжди має викоикатися super()
         super();
-    }
-    setValue(val) {
-        this.value = val;
+        this.testB = b;
     }
 }
-let textBox = new TextBox();
-textBox.setValue('string');
-let checkBox = new CheckBox();
-checkBox.setValue(true);
+const exampleA2 = new ExampleA('a2');
+const exampleA1 = new ExampleA('a1');
+const exampleA3 = new ExampleA('a3');
+const exampleB1 = new ExampleB('b1');
+const exampleB2 = new ExampleB('b2');
+const exampleB3 = new ExampleB('b3');
+// так як всі вони наслідуються від Example то в масиві будуть значення ExampleA і ExampleB
+const collection = [];
+// можна багато параметрів передавати в push
+collection.push(exampleA2, exampleA1, exampleA3, exampleB1, exampleB2, exampleB3);
+// f:Function передаємо батьківський клас
+function filter(f) {
+    for (let i = 0; i < collection.length; i++) {
+        const item = collection[i];
+        if (item instanceof f) {
+            console.log(`item==typeof ${f.prototype.constructor.name}`, item);
+        }
+    }
+}
+// таким чином можна переобразувати типи (має значення якщо типи мають якийсь звязок)
+filter(ExampleA);
+// filter(ExampleB)
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,52 @@
-abstract class Control{
-    // тут буде поліморфізм і ми в дочірніх класах ставимо вже типи
-    protected abstract value:any
-    constructor(){}
-    // тут буде поліморфізм і ми в дочірніх класах ставимо вже типи
-    public abstract setValue(val:any):void
-
-
+// приведення  типів інстансів класів можна робити через instaceof 
+// instanceof для інтерфейсів не працює так як в js немає інтерфейсів
+abstract class Example{
+    public name:string
+    public count:number
+    public convert():string{
+        return `name ${this.name} count=${this.count}`
+    }
 }
-// текстбокси працюють тільки з string
-class TextBox extends Control{
-    protected value: string
-    constructor(){
+class ExampleA extends Example{
+    public testA:string
+    constructor(a:string){
         super()
+        this.testA=a
     }
-    public setValue(val: string): void {
-        this.value=val
-    }
+    
 }
-// чекбокси працюють тільки з boolean
-class CheckBox extends Control{
-    protected value: boolean
-    constructor(){
+class ExampleB extends Example{
+    public testB:string
+    constructor(b:string){
+        // в конструкторі потомків завжди має викоикатися super()
         super()
+        this.testB=b
     }
-    public setValue(val: boolean): void {
-        this.value=val
-    }
+
 }
 
-let textBox=new TextBox()
-textBox.setValue('string')
-let checkBox=new CheckBox()
-checkBox.setValue(true)
+const exampleA2=new ExampleA('a2')
+const exampleA1=new ExampleA('a1')
+const exampleA3=new ExampleA('a3')
+const exampleB1=new ExampleB('b1')
+const exampleB2=new ExampleB('b2')
+const exampleB3=new ExampleB('b3')
+
+// так як всі вони наслідуються від Example то в масиві будуть значення ExampleA і ExampleB
+const collection:Example[]=[]
+// можна багато параметрів передавати в push
+collection.push(exampleA2,exampleA1,exampleA3,exampleB1,exampleB2,exampleB3)
+
+// f:Function передаємо батьківський клас
+function filter(f:Function) {
+    for(let i=0;i<collection.length;i++){
+    const item=collection[i]
+    if(item instanceof f){
+        console.log(`item==typeof ${f.prototype.constructor.name}`,item);
+        
+    }
+}
+}
+// таким чином можна переобразувати типи (має значення якщо типи мають якийсь звязок)
+filter(ExampleA)
+// filter(ExampleB)
