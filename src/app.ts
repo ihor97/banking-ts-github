7fcfda3762dd0f22d587e9bdb2408b433eceb246
ihor97 <daniel.waffner@gmail.com>
2023-06-17 10:55:03 +0100

    типи зіставлення Required

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,8 @@
-class Model<T>{
-    constructor(private entity:T){
-
-    }
-    public getValueKey<U extends keyof T>(key : U):T[U]{
-        return this.entity[key]
-    }
-
-    public update(partial: Partial<T>):void{
-        Object.assign(this.entity as {},partial)
-    }
+interface IExamle{
+    name?:string
+    count?:number
+    region?:string
 }
-interface IAnimalEntity{
-    name:string
-    age:number
-}
-
-let json='{"name":"animal","age":0}'
-let entity=JSON.parse(json)
-let animalModel:Model<IAnimalEntity>=new Model(entity)
-console.log(animalModel);
-
-console.log(animalModel.getValueKey('name'));
-
-let newjson='{"name":"newanimal"}'
-let newentity=JSON.parse(newjson)
-animalModel.update(newentity)
-console.log(animalModel.getValueKey('name'));
-console.log(animalModel);
-
+// робить усі поля обовязковими
+type IExamleRequired=Required<IExamle>
+let h:IExamleRequired
