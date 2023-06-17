7e338656e838321097b2ed0a6d9d3c5bd1fed738
ihor97 <daniel.waffner@gmail.com>
2023-06-17 10:42:01 +0100

    типи зіставлення Omit

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,5 @@
-let os = 'Linux';
-let g = "count";
-// -------------------------------------------------//
-//keyof вертає ключі також з нетипізованого обєкта
-const bmw = { name: 'BMW', power: '1003hp' };
-let h = "name";
-// -------------------------------------------------------//
-// // дістаємо ключі енума
-var ColorsEnum;
-(function (ColorsEnum) {
-    ColorsEnum["white"] = "#ffffff";
-    ColorsEnum["black"] = "#000000";
-})(ColorsEnum || (ColorsEnum = {}));
-let h1 = "black";
+// типи зіставлення 
+// Omit
+let h;
+h = { count: 1, region: '' };
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,12 @@
-
-//// літерал строка по суті enum  
-// // може бути тільки два значення 'Linux'|'Windows'
-type OperationsSystem='Linux'|'Windows'
-let os:OperationsSystem='Linux'
-
-
-// ---------------------------------------------//
+// типи зіставлення 
+// Omit
 
 interface IExamle{
     name:string
     count:number
     region:string
 }
-
-// тут keyof працює як літерал віе верне всі назви полів інтерфейсу IExamle
-type IExamleType=keyof IExamle //"count" | 'name' | 'region' вийде такий результат
-
-let g:IExamleType="count"
-
-// -------------------------------------------------//
-
-//keyof вертає ключі також з нетипізованого обєкта
-const bmw={name:'BMW',power:'1003hp'}
-// можна получити поля обєкта 
-type CarLiteralType=keyof typeof bmw
-let h:CarLiteralType="name"
-
-// -------------------------------------------------------//
-// // дістаємо ключі енума
-enum ColorsEnum{
-    white='#ffffff',
-    black='#000000'
-}
-type CarLiteralType2=keyof typeof ColorsEnum
-let h1:CarLiteralType2="black"
+// Omit вказує що треба вичеркнути
+type ExampleOmitType=Omit<IExamle,'name'>
+let h:ExampleOmitType
+h={count:1,region:''}
