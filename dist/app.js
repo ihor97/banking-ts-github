364f42a45686973fb5c39a2d056127edb187db07
ihor97 <daniel.waffner@gmail.com>
2023-06-17 11:20:26 +0100

    створення типу зі спільними ключами

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,4 @@
+// generic класи є для того щоб міняти типи 
+// Omit Partial .... вже готові рішення для роботи з типами 
+let h = { count: 1, name: '' };
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
+// generic класи є для того щоб міняти типи 
+// Omit Partial .... вже готові рішення для роботи з типами 
 
-interface IExamle{
+type GetObjDifferentKeys<T,U>=Omit<T,keyof U>&Omit<U,keyof T>
+// вертає спільні ключі  обєднання ь=типів T|U тобто створюється спільний тип
+type GetObjSameKeys<T,U>=Omit<T|U,keyof GetObjDifferentKeys<T,U>>
+
+interface IExample2{
+    name:string
+    count:number
+    country:string
+}
+interface IExample1{
     name:string
     count:number
     region:string
 }
-// робить навпаки як omit тобто після коми вказуємо те що має бути а все решта відкидається
-type IExamleRequired=Pick<IExamle,'name'|'count'>
+type IExampleConcat=GetObjSameKeys<IExample2,IExample1>
+let h:IExampleConcat={count:1,name:''}
