0aa4318d5bde6ef0071606a2cbde09108ad3def6
ihor97 <daniel.waffner@gmail.com>
2023-06-17 10:18:25 +0100

    прийом двох дженериків

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,22 @@
+// клас IdentityClass є типу Т який наслідує параметри з інтрефейсу IExampleTemp та має бути сконструйований по GenericInterface
+// клас з двома шаблонними типами
 class IdentityClass {
-    constructor(value) {
+    constructor(value, e) {
         this.value = value;
+        this.e = e;
     }
     getIdentity() {
         return this.value;
     }
+    generate(e) {
+    }
 }
-const myNum = new IdentityClass(4);
-const myStr = new IdentityClass('ddd');
+class ExampleTemp {
+}
+const obj = {
+    temp: '123'
+};
+const et = new ExampleTemp();
+const myNum = new IdentityClass(et, obj);
+console.log(myNum);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,38 @@ interface GenericInterface<Z>{
     value:Z
     getIdentity:()=>Z
 }
-class IdentityClass<T> implements GenericInterface<T>{
+
+interface IExampleTemp{
+    count:number
+}
+interface IGenerate{
+    temp:string
+}
+// клас IdentityClass є типу Т який наслідує параметри з інтрефейсу IExampleTemp та має бути сконструйований по GenericInterface
+// клас з двома шаблонними типами
+class IdentityClass<T extends IExampleTemp,R extends IGenerate> implements GenericInterface<T>{
     public value: T
-    constructor(value:T){
+    public e:R
+    constructor(value:T,e:R){
         this.value=value
+        this.e=e
     }
     public getIdentity () :T{
         return this.value
     }
+    public generate(e:R){
 
+    }
 }
 
-const myNum=new IdentityClass<number>(4)
-const myStr=new IdentityClass<string>('ddd')
+class ExampleTemp implements IExampleTemp{
+    public count: number 
+}
+
+const obj:IGenerate={
+    temp:'123'
+}
+
+const et=new ExampleTemp()
+const myNum=new IdentityClass<ExampleTemp,IGenerate>(et,obj)
+console.log(myNum);
