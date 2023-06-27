3498e8a1a5591d1c61349d8fbbe724630b14e470
ihor97 <daniel.waffner@gmail.com>
2023-06-27 19:03:27 +0100

    декоратор приклад 1

--- dist/app.js
+++ dist/app.js
@@ -1,56 +1,35 @@
-// generics -  потрібно для привязки метода до типу якого ми вкажемо
-// type A={key1:string,key2:string}
-// type B={key2:null,key3:string}
-// // так як key2 не є однаковим  буде вибивати помилку і працювати не буде 
-// type C=A & B
-// let f:C
-// ----------------------------------------//
-// interface IExample{
-//     name:string
-//     count:number
-//     region:string
-// }
-// // створюємо літерал з IExampleType
-// type IExampleType=keyof IExample
-// let C:IExampleType
-// C="region"
-// -------------------==/
-// беремо ключі з різних обєктів
-// type A={key1:string,key2:string}
-// type B={key2:null,key3:string}
-// type getObjDiffKeys<T,U>=Omit<T, keyof U> & Omit<U, keyof T>
-// type ab=getObjDiffKeys<A,B>
-// let c:ab={key1:'s',key3:'ss'}
-// -------------------------------------//
-// interface IExample{
-//     name:string
-//     count:number
-//     region:string
-// }
-// // робить поля не обовязковими
-// type IExampleType=Partial<IExample>
-// let C:IExampleType
-// ---------------------------------------------//
-// interface IExample{
-//     name:string
-//     count:number
-//     region:string
-// }
-// // вибираємо  поля вказані після коми
-// type IExampleType=Pick<IExample, 'name'| 'count'>
-// -------------------------------------------//
-// class Example{
-//     foo:string
-// }
-// class ExampleChild extends Example{
-// }
-// class ExampleOther{
-// }
-// // якщо тип Т наслідується від Example буде foo | bar тип
-// type GenExample<T>=T extends Example ? 'foo' | 'bar':'qwe'
-// let h:GenExample<ExampleChild>
-// по суті дженеріки як функції
-// let b:GenExample<ExampleOther>
-// h="bar"
-// b="qwe"
+class ConcreteComponent {
+    operation() {
+        return 'ConcreteComponent';
+    }
+}
+class Decorator {
+    constructor(component) {
+        this.component = component;
+    }
+    operation() {
+        return this.component.operation();
+    }
+}
+class ConcreteDecoratorA extends Decorator {
+    operation() {
+        return `ConcreteDecoratorA(${super.operation()})`;
+    }
+}
+class ConcreteDecoratorB extends Decorator {
+    operation() {
+        return `ConcreteDecoratorB(${super.operation()})`;
+    }
+}
+function clientCode(comp) {
+    console.log(`Result:${comp.operation()}`);
+}
+const simple = new ConcreteComponent();
+clientCode(simple);
+// тут ми закидуючи аргументом в цей клас викликаємо 
+const decor1 = new ConcreteDecoratorA(simple);
+clientCode(decor1);
+// можна обгортати декоратори 
+const decorator2 = new ConcreteDecoratorB(decor1);
+clientCode(decorator2);
 //# sourceMappingURL=app.js.map
