cf4a3978e1de55cd3e2ee3d04e311f97b0cc61ee
ihor97 <daniel.waffner@gmail.com>
2023-06-28 19:49:26 +0100

    open closed

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,90 @@
-"use strict";
-let g = 5;
+// Open closed principle
+// класи відкриті для розширення але закриті для модифікацій
+var Shape;
+(function (Shape) {
+    Shape["square"] = "SQUARE";
+    Shape["circle"] = "CIRCLE";
+})(Shape || (Shape = {}));
+var incorrect;
+(function (incorrect) {
+    class Square {
+        constructor(sideSize) {
+            this._type = Shape.square;
+            this._sideSize = sideSize;
+        }
+        get sideSize() {
+            return this._sideSize;
+        }
+        get type() {
+            return this._type;
+        }
+    }
+    class Circle {
+        constructor(sideSize) {
+            this._type = Shape.circle;
+            this._radiusSize = sideSize;
+        }
+        get sideSize() {
+            return this._radiusSize;
+        }
+        get type() {
+            return this._type;
+        }
+    }
+    class Calculator {
+        constructor(shapes) {
+            this._shapes = [];
+            this._shapes = shapes;
+        }
+        calculateAllAreas() {
+            return this._shapes.reduce((acc, shape) => {
+                if (shape.type === Shape.circle) {
+                    acc += Math.PI * shape.sideSize ** 2;
+                }
+                // якщо ми захочемо додати якусь фігури треба буде давати ще один if
+                if (shape.type === Shape.square) {
+                    acc += shape.sideSize ** 2;
+                }
+                return acc;
+            }, 0);
+        }
+    }
+    const res = new Calculator([new Square(5), new Circle(0)]);
+    console.log(res.calculateAllAreas());
+})(incorrect || (incorrect = {}));
+var correct;
+(function (correct) {
+    class Shape {
+    }
+    class Circle extends Shape {
+        constructor(sideSize) {
+            super();
+            this._sideSize = sideSize;
+        }
+        getArea() {
+            return Math.PI * this._sideSize ** 2;
+        }
+    }
+    class Square extends Shape {
+        constructor(sideSize) {
+            super();
+            this._sideSize = sideSize;
+        }
+        getArea() {
+            return this._sideSize ** 2;
+        }
+    }
+    class AreaCalculator {
+        constructor(shapes) {
+            this.shapes = [];
+            this.shapes = shapes;
+        }
+        calculateAllAreas() {
+            this.sum = this.shapes.reduce((acc, e) => acc += e.getArea(), 0);
+            return this.sum;
+        }
+    }
+    const res = new AreaCalculator([new Square(5), new Circle(0)]);
+    console.log(res.calculateAllAreas());
+})(correct || (correct = {}));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,120 @@
-// single responsibility кожен клас має відповідати за якусь одну операцію
-// вказує на те що в класу має бути тільки одна вісь змін
+// Open closed principle
+// класи відкриті для розширення але закриті для модифікацій
+
+enum Shape {
+    square='SQUARE',
+    circle="CIRCLE"
+}
+namespace incorrect{
+    class Square{
+        private _sideSize:number
+        private _type=Shape.square
+        constructor(sideSize){
+            this._sideSize=sideSize
+        }
+        public get sideSize():number{
+            return this._sideSize
+        }
+        public get type():Shape{
+            return this._type
+        }
+    }
+    class Circle{
+        private _radiusSize:number
+        private _type=Shape.circle
+        constructor(sideSize){
+            this._radiusSize=sideSize
+        }
+        public get sideSize():number{
+            return this._radiusSize
+        }
+        public get type():Shape{
+            return this._type
+        }
+    }
+
+    class Calculator{
+        private _shapes:any[]=[]
+        constructor(shapes:any[]){
+            this._shapes=shapes
+        }
+        public calculateAllAreas():number{
+            return this._shapes.reduce((acc,shape)=>{
+
+                if(shape.type===Shape.circle){
+                     acc+=Math.PI*shape.sideSize**2
+                }
+                // якщо ми захочемо додати якусь фігури треба буде давати ще один if
+                if(shape.type===Shape.square){
+                     acc+=shape.sideSize**2
+                }
+                return acc
+            },0)
+        }
+    }
+
+
+    const res = new Calculator([new Square(5),new Circle(0)])
+
+    console.log(res.calculateAllAreas());
+    
+
+}
+
+
+namespace correct{
+
+
+
+abstract class Shape {
+    abstract getArea():number
+}
+
+class Circle extends Shape{
+    private _sideSize:number
+    constructor(sideSize){
+        super()
+        this._sideSize=sideSize
+    }
+  getArea(): number {
+      return Math.PI*this._sideSize**2
+  }
+}
+class Square extends Shape{
+    private _sideSize:number
+    constructor(sideSize){
+        super()
+        this._sideSize=sideSize
+    }
+  getArea(): number {
+      return this._sideSize**2
+  }
+}
+class AreaCalculator{
+    shapes:Shape[]=[]
+    sum:number
+
+    constructor(shapes:Shape[]){
+        this.shapes=shapes
+    }
+    calculateAllAreas(){
+        this.sum=this.shapes.reduce((acc,e)=>acc+=e.getArea(),0)
+        return this.sum
+    }
+}
+
+
+const res = new AreaCalculator([new Square(5),new Circle(0)])
+
+console.log(res.calculateAllAreas());
+
+
+
+
+
+
+
+
+
+
+}
