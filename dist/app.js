86b09c587a01028fd61d979754f5aee8e74946ef
ihor97 <daniel.waffner@gmail.com>
2023-06-28 21:27:46 +0100

    dependency inversion

--- dist/app.js
+++ dist/app.js
@@ -1,39 +1,50 @@
-// interface segregation principle
-// прицип служить для того щоб могла бути постійність: клас батько і клас потомок можуть використовуватися однаковим чином без нарушення роботи програми
-// типу як single responsibility але в плані інтерфейсів (не треба пхати всі аідряд методи в інтерфейс )
+// Dependency Inversion -
+// модули верхнього рівня не повинні заледати від модулів нижнього рівня. ті і інші мусять залежати від абстрацій
+//  Абстракції не повинні залежати від деталей, деталі мусять залежати від абстракцій
+// в нас може бути клас User і є клас Логер який відповідає за те щоб зберігати помилки 
+// логер це не є наша бізнес логіка він потрібен нам а не апці щоб вона працювала
+// тобто високорівнева політика не має залежати від таких класів як логер
+// щоб вирішити це ми створемо інтерфейс ILogger User буде залежати від інтерфейса
 var incorrect;
 (function (incorrect) {
-    class RegularProgrammer {
-        writeCode() {
-            console.log('I write code');
-        }
-        eatPizza(slicesCount) {
-            console.log('eating');
+    class Clioutput {
+        print(message) {
+            console.log(message);
         }
     }
-    class FreeLancer {
-        writeCode() {
-            console.log('I write code');
-        }
-        eatPizza(slicesCount) {
-            throw new Error('не можу!');
+    class Cat {
+        meow() {
+            const cliOutput = new Clioutput();
+            cliOutput.print('meow');
         }
     }
+    const cat = new Cat();
+    cat.meow();
 })(incorrect || (incorrect = {}));
 var correct;
 (function (correct) {
-    class RegularProgrammer {
-        writeCode() {
-            console.log('I write code');
-        }
-        eatPizza(slicesCount) {
-            console.log('eating');
+    class Clioutput {
+        print(message) {
+            console.log(message);
         }
     }
-    class FreeLancer {
-        writeCode() {
-            console.log('I write code');
+    class Screenoutput {
+        print(message) {
+            // logic 
         }
     }
+    class Cat {
+        // тобто код залежить від інтерфейса
+        constructor(output) {
+            this.output = output;
+        }
+        meow() {
+            this.output.print('meow');
+        }
+    }
+    // в аннуларі є isDevMode()
+    // тобто ми в клас кота не ліземо 
+    // const cat = new Cat( isDevMode() ? new Clioutput() : new Screenoutput())
+    const cat = new Cat(new Clioutput());
 })(correct || (correct = {}));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,74 @@
-// interface segregation principle
-// прицип служить для того щоб могла бути постійність: клас батько і клас потомок можуть використовуватися однаковим чином без нарушення роботи програми
-// типу як single responsibility але в плані інтерфейсів (не треба пхати всі аідряд методи в інтерфейс )
+// Dependency Inversion -
+// модули верхнього рівня не повинні заледати від модулів нижнього рівня. ті і інші мусять залежати від абстрацій
+//  Абстракції не повинні залежати від деталей, деталі мусять залежати від абстракцій
+// в нас може бути клас User і є клас Логер який відповідає за те щоб зберігати помилки 
+// логер це не є наша бізнес логіка він потрібен нам а не апці щоб вона працювала
+// тобто високорівнева політика не має залежати від таких класів як логер
+// щоб вирішити це ми створемо інтерфейс ILogger User буде залежати від інтерфейса
 
-namespace incorrect{
-interface Programmer{
-    writeCode():void
-    eatPizza(slicesCount:number):void
-}
+namespace incorrect {
 
-class RegularProgrammer implements Programmer{
-    public writeCode(): void {
-        console.log('I write code');
-    }
-    public eatPizza(slicesCount: number): void {
-        console.log( 'eating');
-        
-    }
-}
-    class FreeLancer implements Programmer {
-        public writeCode(): void {
-            console.log('I write code');
-        }
-        public eatPizza(slicesCount: number): void {
-            throw new Error('не можу!')
-            
+    class Clioutput {
+        public print(message): void {
+            console.log(message);
+
         }
     }
 
-
-
-
-
-}
-
-
-namespace correct{
-
-interface CodeProducer{
-    writeCode():void
-}
-interface PizzaEater{
-    eatPizza(slicesCount: number):void
-}
-
-class RegularProgrammer implements CodeProducer,PizzaEater{
-    public writeCode(): void {
-        console.log('I write code');
-    }
-    public eatPizza(slicesCount: number): void {
-        console.log( 'eating');
-        
-    }
-}
-    class FreeLancer implements CodeProducer {
-        public writeCode(): void {
-            console.log('I write code');
+    class Cat {
+        public meow(): void {
+            const cliOutput = new Clioutput()
+            cliOutput.print('meow')
         }
-        
     }
+
+    const cat = new Cat()
+    cat.meow()
+
+
+
+
+
+
+
+
+}
+
+
+namespace correct {
+// тобто тут робимо таку залежність Cat -> IOutput <- Clioutput
+    interface IOutput {
+        print(message: string): void
+    }
+
+    class Clioutput implements IOutput {
+        public print(message): void {
+            console.log(message);
+
+        }
+    }
+    class Screenoutput implements IOutput{
+        public print(message: string): void {
+            // logic 
+        }
+    }
+
+    class Cat {
+        // тобто код залежить від інтерфейса
+        constructor(private output: IOutput) { }
+
+        public meow(): void {
+            this.output.print('meow')
+        }
+    }
+// в аннуларі є isDevMode()
+// тобто ми в клас кота не ліземо 
+    // const cat = new Cat( isDevMode() ? new Clioutput() : new Screenoutput())
+    const cat = new Cat(new Clioutput())
+
+
+
+
 
 
 }
