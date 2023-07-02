cacbe1b7c93ad00d66d0f949d3973ca932a7b93b
ihor97 <daniel.waffner@gmail.com>
2023-07-02 09:09:50 +0100

    takeUntil

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
+const event$ = new rxjs_1.Subject();
 (0, rxjs_1.interval)(1000)
-    // отримує перше значення і відписується
-    .pipe((0, rxjs_1.first)())
+    // takeUntil треба для того зоб відписуватися по умові (як дестрой компоненти)
+    // він приймає в себе якийсб іміт (як правило Subject) як тільки Subject видасть значення підписка знищиться
+    .pipe((0, rxjs_1.takeUntil)(event$))
     .subscribe(console.log);
+setTimeout(() => {
+    event$.next(1);
+}, 2000);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
-import { interval, take,skip, first } from "rxjs";
+import { interval, take,skip, first, takeUntil, Subject } from "rxjs";
 
-
+ const event$=new Subject()
 interval(1000)
-// отримує перше значення і відписується
-.pipe(first())
+// takeUntil треба для того зоб відписуватися по умові (як дестрой компоненти)
+// він приймає в себе якийсб іміт (як правило Subject) як тільки Subject видасть значення підписка знищиться
+.pipe(takeUntil(event$))
 .subscribe(console.log)
 
 
+setTimeout(() => {
+    event$.next(1)
+}, 2000);
 
 
 
