e679e15bda3f5ea7f5ac73b6577f7706ba5e2b37
ihor97 <daniel.waffner@gmail.com>
2023-07-02 08:24:59 +0100

    приклад

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
-// тут уже буде
+// гарячому потоку не потрібен підписники щоб видавати значення тому буде пусто
 const Sub = new rxjs_1.Subject();
-Sub.subscribe(e => { console.log(e); });
 Sub.next(1);
 Sub.next(2);
 Sub.next(3);
+Sub.subscribe(e => { console.log(e); });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import { subscribe } from 'diagnostics_channel';
 import { Observable, Subject, filter, map, of, tap } from 'rxjs';
-// тут уже буде
+// гарячому потоку не потрібен підписники щоб видавати значення тому буде пусто
 const Sub=new Subject()
-Sub.subscribe(e=>{console.log(e);})
 Sub.next(1)
 Sub.next(2)
 Sub.next(3)
+Sub.subscribe(e=>{console.log(e);})
