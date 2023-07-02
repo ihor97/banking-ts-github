bd62fbc588c787fcc2ef92f553d33d192c04c3a2
ihor97 <daniel.waffner@gmail.com>
2023-07-02 10:46:19 +0100

    concat

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
+// комьінування обєднує декілька потоків в один
 const observable = new rxjs_1.Observable(sub => {
     sub.next(1);
-    sub.error('error');
+    sub.next(4);
+    sub.next(6);
+    sub.complete();
 });
-observable.pipe(
-// ще одна фіча як можна юзати tap 
-(0, rxjs_1.tap)(val => console.log(val), err => console.log(err), () => console.log('complete'))).subscribe({ error(err) {
-        console.log(err);
-    } });
+// конкат склуює декілька потоків в одни
+// щоб concat перейшов до наступного потоку треба щоб попередній закомплітився
+(0, rxjs_1.concat)(observable, (0, rxjs_1.timer)(1000), (0, rxjs_1.timer)(2000), (0, rxjs_1.of)(1, 2, 3)).subscribe(val => console.log(val));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import { interval, take,skip, tap, takeUntil, of, Observable, catchError } from "rxjs";
-
+import { interval, take,skip, tap, takeUntil, of, Observable, catchError, concat, timer } from "rxjs";
+// комьінування обєднує декілька потоків в один
 const observable=new Observable(sub=>{
     sub.next(1)
-    sub.error('error')
+    sub.next(4)
+    sub.next(6)
+    sub.complete()
+    
 })
-
-observable.pipe(
-    // ще одна фіча як можна юзати tap 
-    tap(
-        val=>console.log(val),
-        err=>console.log(err),
-        ()=>console.log('complete')
-        
-    )
-).subscribe({error(err){console.log(err);
-}});
+// конкат склуює декілька потоків в одни
+// щоб concat перейшов до наступного потоку треба щоб попередній закомплітився
+concat(
+    observable,
+    timer(1000),
+    timer(2000),
+    of(1,2,3)
+).subscribe(val=>console.log(val))
