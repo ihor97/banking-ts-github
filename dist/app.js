1140e1f93492b7ad8914bc2312d9d9f0e811ca2c
ihor97 <daniel.waffner@gmail.com>
2023-07-02 08:21:34 +0100

    приклад

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rxjs_1 = require("rxjs");
-// нічого не виведе тому що потік комплітиться
-const obs = new rxjs_1.Observable(subscriber => {
+// тіло Observable це є ф-я яка виконається тоді коли хтось підпишеться на цей Observable
+// буде 4
+const obs = new rxjs_1.Observable(observer => {
     setTimeout(() => {
-        subscriber.next(2);
-    }, 2000);
-    subscriber.complete();
+        observer.error(4);
+    });
+    observer.next(1);
+    observer.next(2);
 });
-obs.subscribe(console.log);
+// 1 2 1 2 error 4
+obs.subscribe(e => console.log(e), err => {
+    obs.subscribe(num => console.log(num));
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import { subscribe } from 'diagnostics_channel';
-import { Observable, map, of, tap } from 'rxjs';
+import { Observable, filter, map, of, tap } from 'rxjs';
 
-// нічого не виведе тому що потік комплітиться
-const obs=new Observable(subscriber=>{
+// тіло Observable це є ф-я яка виконається тоді коли хтось підпишеться на цей Observable
+// буде 4
+const obs = new Observable(observer => {
     setTimeout(() => {
-        subscriber.next(2)
-    }, 2000);
-
-    subscriber.complete()
+        observer.error(4)
+    });
+    observer.next(1)
+    observer.next(2)
 })
-obs.subscribe(console.log
+// 1 2 1 2 error 4
+obs.subscribe(
+    e => console.log(e),
+    err => {
+        obs.subscribe(
+            num => console.log(num)
+        )
+    }
+
 )
