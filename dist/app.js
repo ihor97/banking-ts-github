908bcd876035b7c23d423e59dc3d240c3035f3fa
ihor97 <daniel.waffner@gmail.com>
2023-07-08 13:10:14 +0100

    декоратор класу і методу порівняння

--- dist/app.js
+++ dist/app.js
@@ -1,64 +1,19 @@
-// реалізація менеджера запущених процесів
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-class TaskRunner {
-    static register(task) {
-        if (!this._collection.filter(g => g.taskName === task.taskName).length) {
-            this._collection.push(task);
-            return true;
-        }
-        return false;
-    }
-    static getAll() {
-        return this._collection;
-    }
+// декоратор метода
+// propKey - назва методу в класі
+function decoratorMethod(target, propKey, descriptor) {
+    // обєкт в якому будемо міняти метод 
+    console.log(target);
+    // назва метода в класі
+    console.log(propKey);
+    // інформація про поле
+    console.log(descriptor);
+    return descriptor;
 }
-TaskRunner._collection = [];
-// кожен раз коли створюється екземпляр класу відпрацьовує декоратор
-// в момент коли створюється задачка ми її реєструємо в класі TaskRunner
-function registerTask(target) {
-    let newController = function () {
-        console.log('створений новий процес');
-        let index = 0;
-        while (true) {
-            index++;
-            this.taskName = `задача №${index}`;
-            if (TaskRunner.register(this)) {
-                break;
-            }
-        }
+// декоратор класу
+function decoratorClass(target) {
+    let newConstructor = function () {
+        console.log('exec newConstructor');
     };
-    console.log(newController);
-    // по суті ви вертаємо конструктор який виконується апи ініціадізації класу
-    return newController;
+    return newConstructor;
 }
-// немає силок між класом TaskWorker та TaskRunner але за допомогою декоратора ми реєструємо нових таскворкерів в середині класу TaskRunner
-let TaskWorker = class TaskWorker {
-    constructor() {
-        this.taskName = '';
-        this.running = false;
-    }
-    start() {
-        this.running = true;
-        console.log(`Запущена ${this.taskName}`);
-    }
-    pause() {
-        this.running = false;
-        console.log(`Приоставлена ${this.taskName}`);
-    }
-};
-TaskWorker = __decorate([
-    registerTask
-], TaskWorker);
-let task1 = new TaskWorker();
-let task2 = new TaskWorker();
-let task3 = new TaskWorker();
-let task4 = new TaskWorker();
-let task5 = new TaskWorker();
-console.log(TaskRunner.getAll());
-task1.pause();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,22 @@
-// реалізація менеджера запущених процесів
-
-class TaskRunner{
-    private static _collection:TaskWorker[]=[]
-    public static register(task:TaskWorker){
-        if(!this._collection.filter(g=>g.taskName===task.taskName).length){
-            this._collection.push(task)
-        return true
-
-        }
-        return false
-    }
-    public static getAll():Array<TaskWorker>{
-        return this._collection
-    }
+// декоратор метода
+// propKey - назва методу в класі
+function decoratorMethod(target:Object,propKey:string,
+    descriptor:TypedPropertyDescriptor<any>):TypedPropertyDescriptor<any> {
+    // обєкт в якому будемо міняти метод 
+    console.log(target);
+// назва метода в класі
+    console.log(propKey);
+    // інформація про поле
+    console.log(descriptor);
+    return descriptor
 }
-// кожен раз коли створюється екземпляр класу відпрацьовує декоратор
-// в момент коли створюється задачка ми її реєструємо в класі TaskRunner
-function registerTask<TFunction extends Function>(target:TFunction):TFunction {
-    let newController:Function=function () {
-        console.log('створений новий процес');
-        let index=0
-        while(true){
-            index++
-            this.taskName=`задача №${index}` 
-            if(TaskRunner.register(this)){
-                break
-            }
-        }
+
+// декоратор класу
+
+function decoratorClass<TFunction extends Function>(target :TFunction ):TFunction {
+    let newConstructor:Function=function () {
+        console.log('exec newConstructor');
         
     }
-    console.log(newController);
-    
-    // по суті ви вертаємо конструктор який виконується апи ініціадізації класу
-    return <TFunction>newController
+    return <TFunction>newConstructor
 }
-// немає силок між класом TaskWorker та TaskRunner але за допомогою декоратора ми реєструємо нових таскворкерів в середині класу TaskRunner
-@registerTask
-class TaskWorker{
-    public taskName:string=''
-    public running:boolean=false
-    constructor(){}
-
-    public start():void{
-        this.running=true
-        console.log(`Запущена ${this.taskName}`);
-    }
-    public pause(){
-        this.running=false
-        console.log(`Приоставлена ${this.taskName}`);
-        
-    }
-
-
-}
-let task1=new TaskWorker()
-let task2=new TaskWorker()
-let task3=new TaskWorker()
-let task4=new TaskWorker()
-let task5=new TaskWorker()
-console.log(TaskRunner.getAll());
-task1.pause()
