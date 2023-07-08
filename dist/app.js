484417524f24f7e4b45fed12f83bec29aba22d40
ihor97 <daniel.waffner@gmail.com>
2023-07-08 19:14:23 +0100

    dependency injection

--- dist/app.js
+++ dist/app.js
@@ -1,35 +1,74 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function decoratorProperty(target, propKey) {
-    let i = 0;
-    //  приклад з гет і сет
-    let descriptor = {
+Object.defineProperty(exports, "__esModule", { value: true });
+// dependency injection дозволяє розграничити функціонал аплікухи
+require("reflect-metadata");
+class PrintService {
+}
+class PrintDocumentService extends PrintService {
+    writeMessage(message) {
+        document.write(message);
+    }
+}
+class PrintConsoleService extends PrintService {
+    writeMessage(message) {
+        console.log(message);
+    }
+}
+class PrintLocaleStorageService extends PrintService {
+    writeMessage(message) {
+        localStorage.setItem('message', message);
+    }
+}
+class ServiceLocator {
+    // interfaceType тип реєструючої залежності
+    // instance екземпляр класа який їй відповідає
+    static registerService(interfaceType, instance) {
+        let record = this._sevices.find(x => x.interfaceType === interfaceType);
+        if (!record) {
+            record = { interfaceType: interfaceType, instance: instance };
+            this._sevices.push(record);
+        }
+        else {
+            record.instance = instance;
+        }
+    }
+    static getService(interfaceType) {
+        // When you use this approach, you basically tell TypeScript that this value will never be null or undefined. використовуємо !
+        //  тут помилка!!!!!!!!!!!!
+        return this._sevices.find(x => x.interfaceType == interfaceType).instance;
+    }
+}
+ServiceLocator._sevices = [];
+ServiceLocator.registerService(PrintService, new PrintDocumentService());
+// ServiceLocator.registerService(PrintService,new PrintConsoleService())
+// ServiceLocator.registerService(PrintService,new PrintLocaleStorageService())
+function inject(target, propKey) {
+    let propType = Reflect.getMetadata("design:type", target, propKey);
+    let printService = ServiceLocator.getService(propType);
+    // треба скачати reflect-metadata
+    // рефлексія -зміна структури обєкта в ході виконання програми
+    var descriptor = {
         get: function () {
-            i++;
-            return i;
-        },
-        set: function (val) {
-            i = val;
+            return printService;
         }
     };
     return descriptor;
 }
 class Example {
-    constructor() {
-        console.log(this._a);
-        console.log(this._a);
-        console.log(this._a);
-        console.log(this._a);
-        this._a = 10;
-        console.log(this._a);
+    constructor() { }
+    print(message) {
+        this._printService.writeMessage(message);
     }
 }
 __decorate([
-    decoratorProperty
-], Example.prototype, "_a", void 0);
+    inject
+], Example.prototype, "_printService", void 0);
 let e = new Example();
+e.print('first message');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,77 @@
+// dependency injection дозволяє розграничити функціонал аплікухи
+import 'reflect-metadata';
 
-function decoratorProperty(target:Object,propKey:string|symbol):any {
-let i=0
-//  приклад з гет і сет
-let descriptor={
-    get:function () {
-        i++
-        return i
-    },
-    set:function(val:number){
-        i=val
+
+abstract class PrintService {
+        abstract writeMessage(message:string):void
+}
+class PrintDocumentService extends PrintService{
+    public writeMessage(message: string): void {
+        document.write(message)
     }
-}   
+}
+class PrintConsoleService extends PrintService{
+    public writeMessage(message: string): void {
+        console.log(message);
+    }
+}
+class PrintLocaleStorageService extends PrintService{
+    public writeMessage(message: string): void {
+        localStorage.setItem('message',message)
+    }
+}
+
+class ServiceLocator {
+    private static _sevices:[{interfaceType:Function,instance:Object}]=[] as any
+    // interfaceType тип реєструючої залежності
+    // instance екземпляр класа який їй відповідає
+
+    public static registerService(interfaceType:Function,instance:Object){
+        let record=this._sevices.find(x=>x.interfaceType===interfaceType)
+        if(!record){
+            record={interfaceType:interfaceType,instance:instance}
+            this._sevices.push(record)
+        }
+        else{
+            record.instance=instance
+        }
+       
+    }
+    public static getService(interfaceType:Function):Object{
+        // When you use this approach, you basically tell TypeScript that this value will never be null or undefined. використовуємо !
+    //  тут помилка!!!!!!!!!!!!
+        return this._sevices.find(x=>x.interfaceType==interfaceType).instance 
+    }
+}
+
+ServiceLocator.registerService(PrintService,new PrintDocumentService())
+// ServiceLocator.registerService(PrintService,new PrintConsoleService())
+// ServiceLocator.registerService(PrintService,new PrintLocaleStorageService())
+
+function inject(target:Object,propKey:string):any {
+        let propType=Reflect.getMetadata("design:type",target,propKey)
+        let printService=ServiceLocator.getService(propType)
+        // треба скачати reflect-metadata
+        // рефлексія -зміна структури обєкта в ході виконання програми
+    var descriptor={
+        get :function(){
+          return  printService
+        }
+    }
     return descriptor
 }
-
 class Example {
-    // робимо з поля властивість аксесор
-    @decoratorProperty
-    private _a:number
-    constructor() {
-        console.log(this._a);
-        console.log(this._a);
-        console.log(this._a);
-        console.log(this._a);
-        this._a=10
-        console.log(this._a);
-        
-        
+    // осеовна ціль внедріння залежності в тому що ми не використовуємо конструктор і вказуємо фреймворку що покласти в даний обєкт який саме екземпляр реалізації абстрактного класу туди покласти
+    @inject
+    private _printService:PrintService
+
+    constructor(){}
+
+    public print(message:string){
+        this._printService.writeMessage(message)
     }
+
 }
 
-let e=new Example()
-
+let e =new Example()
+e.print('first message')
