481f67c494fa7bef8c0a225acf30c45d1ccbce15
ihor97 <daniel.waffner@gmail.com>
2023-07-08 18:51:34 +0100

    декоратор поля

--- dist/app.js
+++ dist/app.js
@@ -1,63 +1,6 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-let key = '[variable]';
-let template = `_method_${key}_parametrs`;
-let metadata = {};
-// фабрика декораторів  - вертає декоратор  резолвить якусь нову ф-ю
-function decorateParametr() {
-    return (target, method, index) => {
-        // 
-        let metadataKey = template.replace(key, method);
-        if (!metadata[metadataKey]) {
-            metadata[metadataKey] = [];
-        }
-        metadata[metadataKey].push(index);
-    };
+// декоратори полів
+function decoratorProperty(target, propKey) {
+    let descriptor = {};
+    return descriptor;
 }
-function decorateMethod(str) {
-    return (target, method, descriptor) => {
-        let originalMethod = descriptor.value;
-        descriptor.value = function (...args) {
-            let parametrIndex = template.replace(key, method);
-            var indices = metadata[parametrIndex];
-            let argsWithValue = '';
-            for (var i = 0; i < args.length; i++) {
-                if (indices.indexOf(i) !== -1) {
-                    var arg = args[i];
-                    var argStr = JSON.stringify(arg) || args.toString();
-                    argsWithValue += `arg[${i}]:${argStr}`;
-                }
-            }
-            console.log(`
-            Декоратор с параметром = '${str}' 
-            вызван дял метода '${method}' 
-            параметры ${argsWithValue} 
-            екземпляр класа '${target.constructor.name}' 
-            `);
-            return originalMethod.apply(this, args);
-        };
-    };
-}
-class Example {
-    exec(a, b, c) {
-        this._a = a;
-    }
-}
-__decorate([
-    decorateMethod('кастомний параметр'),
-    __param(0, decorateParametr()),
-    __param(1, decorateParametr()),
-    __param(2, decorateParametr())
-], Example.prototype, "exec", null);
-let tom = new Example();
-tom.exec('Параметр 1', 'Параметр 2', 'Параметр 3');
-tom.exec('Параметр 4', 'Параметр 5', 'Параметр 6');
-tom.exec('Параметр 7', 'Параметр 8', 'Параметр 9');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,8 @@
-let key='[variable]'
-let template=`_method_${key}_parametrs`
-let metadata={}
-// фабрика декораторів  - вертає декоратор  резолвить якусь нову ф-ю
-function decorateParametr():(target:Object,method:string,index:number)=>void {
-    return (target:Object,method:string,index:number)=>{
-        // 
-        let metadataKey=template.replace(key,method)
+// декоратори полів
 
-        if(!metadata[metadataKey]){
-            metadata[metadataKey]=[]
-        }
+function decoratorProperty(target:Object,propKey:string|symbol):any {
+    let descriptor:PropertyDescriptor={
 
-        metadata[metadataKey].push(index)
-    }
+    }    
+    return descriptor
 }
-
-function decorateMethod(str:string):(target:Object,method:string,
-    descriptor:PropertyDescriptor)=>void {
-    return (target:Object,method:string,descriptor:PropertyDescriptor)=>{
-
-        let originalMethod=descriptor.value
-        descriptor.value =function (...args:any[]) {
-            let parametrIndex=template.replace(key,method)
-            var indices=metadata[parametrIndex]
-
-            let argsWithValue=''
-
-            for(var i=0;i<args.length;i++){
-                if(indices.indexOf(i)!==-1){
-                    var arg=args[i]
-                    var argStr=JSON.stringify(arg)|| args.toString()
-                    argsWithValue+=`arg[${i}]:${argStr}`
-                }
-            }
-
-            console.log(`
-            Декоратор с параметром = '${str}' 
-            вызван дял метода '${method}' 
-            параметры ${argsWithValue} 
-            екземпляр класа '${target.constructor.name}' 
-            `);
-            return originalMethod.apply(this,args)
-        }
-    }
-}
-
-class Example{
-    private _a:string
-    @decorateMethod('кастомний параметр')
-    public exec(@decorateParametr() a:string,@decorateParametr()b:string,@decorateParametr()c:string){
-        this._a=a
-    }
-
-}
-
-let tom=new Example()
-tom.exec('Параметр 1','Параметр 2','Параметр 3')
-tom.exec('Параметр 4','Параметр 5','Параметр 6')
-tom.exec('Параметр 7','Параметр 8','Параметр 9')
-
-
