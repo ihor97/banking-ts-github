5b00e597b6a4d6582e5487086bf30883f0940ea0
ihor97 <daniel.waffner@gmail.com>
2023-07-08 18:54:01 +0100

    декоратор поля приклад

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,29 @@
-// декоратори полів
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 function decoratorProperty(target, propKey) {
-    let descriptor = {};
+    let descriptor = {
+        writable: false,
+        value: 'значення встановлене в декораторі'
+    };
     return descriptor;
 }
+class Example {
+    constructor() {
+        // this.a='переіначене значення'
+    }
+}
+__decorate([
+    decoratorProperty
+], Example.prototype, "a", void 0);
+// будемо отримувати одне і те саме поле
+let e = new Example();
+console.log(e.a);
+let e1 = new Example();
+console.log(e1.a);
+let e2 = new Example();
+console.log(e2.a);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
-// декоратори полів
-
 function decoratorProperty(target:Object,propKey:string|symbol):any {
     let descriptor:PropertyDescriptor={
-
+        writable:false,
+        value:'значення встановлене в декораторі'
     }    
     return descriptor
 }
+
+class Example{
+    // таким чином ми зробили статичне readonly поле
+    @decoratorProperty
+    public a:string
+    constructor(){
+        // this.a='переіначене значення'
+        
+    }
+}
+// будемо отримувати одне і те саме поле
+let e =new Example()
+console.log(e.a);
+
+let e1 =new Example()
+console.log(e1.a);
+
+let e2 =new Example()
+console.log(e2.a);
