2dfe01c9baca1e786cdc1508987167d5cc38a238
ihor97 <daniel.waffner@gmail.com>
2023-07-09 13:56:50 +0100

    ще про namespace

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,19 @@
 // кастомний простір імен 
 // типу як в класі є поля так і в namespace є класи
 // дозволяє розграничити області 
+// також можуть бути namespace всередині namespace
 var CustomNameSpace;
 (function (CustomNameSpace) {
     class Example {
     }
     CustomNameSpace.Example = Example;
+    function name(params) {
+    }
+    CustomNameSpace.name = name;
 })(CustomNameSpace || (CustomNameSpace = {}));
 class Example {
 }
+// так можна зробити так щоб не писати багато разів CustomNameSpace
+var b = CustomNameSpace;
 let g = new CustomNameSpace.Example();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 // кастомний простір імен 
 // типу як в класі є поля так і в namespace є класи
 // дозволяє розграничити області 
+// також можуть бути namespace всередині namespace
 namespace CustomNameSpace{
     export class Example {
         
     }
-
+export function name(params) {
     
+}
+    interface IItem{
+        title:string
+    }
 }
 class Example {
         
 }
+// так можна зробити так щоб не писати багато разів CustomNameSpace
+import b=CustomNameSpace
+
+
 let g= new CustomNameSpace.Example()
