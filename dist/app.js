cfe97003c038593a55268a070d4978ced0981cff
ihor97 <daniel.waffner@gmail.com>
2023-07-09 13:29:42 +0100

    вступ

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,13 @@
-"use strict";
-let g = 5;
+// кастомний простір імен 
+// типу як в класі є поля так і в namespace є класи
+// дозволяє розграничити області 
+var CustomNameSpace;
+(function (CustomNameSpace) {
+    class Example {
+    }
+    CustomNameSpace.Example = Example;
+})(CustomNameSpace || (CustomNameSpace = {}));
+class Example {
+}
+let g = new CustomNameSpace.Example();
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+// кастомний простір імен 
+// типу як в класі є поля так і в namespace є класи
+// дозволяє розграничити області 
+namespace CustomNameSpace{
+    export class Example {
+        
+    }
+
+    
+}
+class Example {
+        
+}
+let g= new CustomNameSpace.Example()
